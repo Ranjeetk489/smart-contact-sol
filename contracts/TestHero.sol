ac144e1e9bc3c40cbee2b1f1bfdad15dbd66383b
Ranjeet Kumar <Ranjeet7494@outlook.com>
2022-02-23 09:45:01 +0000

    added another contract

---
+++ contracts/TestHero.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+import "./Hero.sol";
+
+contract TestHero is Hero {
+    uint random;
+    function generateRandom() public override view returns (uint) {
+        return random;
+    }
+    function setRandom(uint r) public {
+        random = r;
+    }
+}
--- test/Hero.ts
+++ test/Hero.ts
@@ -1,29 +1,30 @@
 import "@nomiclabs/hardhat-ethers";
 import { expect } from "chai";
-import {ethers} from "hardhat";
+import { ethers } from "hardhat";
 
-describe("Hero", function() {
+describe("Hero", function () {
     async function createHero() {
-    const Hero = await ethers.getContractFactory("Hero");
-    const hero = await Hero.deploy();
-    await hero.deployed();
+        const Hero = await ethers.getContractFactory("TestHero");
+        const hero = await Hero.deploy();
+        await hero.deployed();
 
-    return hero;
-}
-let hero;
-before(async function(){
-    hero = await createHero();
-});
-it("SHOULD FAIL AT CREATE HERO CAUSE OF PAYMENT", async function () {
-    let e;
-    try{
+        return hero;
+    }
+    let hero;
+    before(async function () {
+        hero = await createHero();
+    });
+    it("should get a zero hero array.", async function () {
+        expect(await hero.getHeroes()).to.deep.equal([]);
+    })
+    it("SHOULD FAIL AT CREATE HERO CAUSE OF PAYMENT", async function () {
+        const hero = await createHero();
+
+        await hero.setRandom(69);
         await hero.createHero(0, {
-            value: ethers.utils.parseEther("0.4999999")
-        })
-    }
-    catch(err){
-        e= err;
-    }
-    expect(e.message.includes("Please send more money")).to.equal(true);
-})
-});
+            value: ethers.utils.parseEther("0.05")
+        });
+        const heroes = (await hero.getHeroes())[0];
+        expect(await hero.getMagic(heroes)).to.equal(16);
+    })
+});  
